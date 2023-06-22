547591a79e9c533e73eb56127b22b85d13dfa3d0
Đinh Đức Dương <97863053+dinhducduong@users.noreply.github.com>
2023-06-22 14:00:33 +0700

    update

--- tap_woocommerce/streams.py
+++ tap_woocommerce/streams.py
@@ -15,23 +15,25 @@ class ProductsStream(WooCommerceStream):
     name = "products"
     path = "products"
     primary_keys = ["id"]
+    records_jsonpath = "$.products[*]"
     replication_key = None
 
     schema = th.PropertiesList(
         th.Property("id", th.IntegerType),
         th.Property("sku", th.StringType),
         th.Property("name", th.StringType),
-        th.Property("price", th.IntegerType),
+        th.Property("price", th.StringType),
         th.Property("options", th.ArrayType(th.ObjectType(
             th.Property("title", th.StringType),
             th.Property("values", th.ArrayType(th.StringType))
         ))),
         th.Property("media_gallery_entries", th.ArrayType(th.ObjectType(
             th.Property("id", th.IntegerType),
-            th.Property("file", th.ArrayType(th.StringType)),
-            th.Property("label", th.ArrayType(th.StringType))
+            th.Property("file", th.StringType),
+            th.Property("label", th.StringType),
         ))),
         th.Property("created_at", th.StringType),
+        th.Property("source", th.StringType),
     ).to_dict()
 
     def parse_response(self, response: requests.Response) -> Iterable[dict]:
@@ -40,97 +42,91 @@ class ProductsStream(WooCommerceStream):
             for item in data:
                 raw_data = {
                     "id": item['id'],
-                    "name": item['title'],
-                    "sku": item['handle'],
-                    "created_at": item['created_at'],
-                    "updated_at": item['updated_at'],
+                    "name": item['name'],
+                    "sku": item['slug'],
+                    "price": item['price'],
+                    "created_at": item['date_created'],
                     "options": [],
                     "media_gallery_entries": [],
-                    "source": "shopify"
+                    "source": "woocommerce"
                 }
-                for variant in item['variants']:
+                for attribute in item['attributes']:
                     raw_data['options'].append({
-                        "product_sku": variant['sku'],
-                        "title": variant['title'],
-                        "price": variant['price'],
-                        "sku": variant['sku'],
-                        "created_at": variant['created_at'],
-                        "updated_at": variant['updated_at'],
+                        "title": attribute['name'],
+                        "values": attribute['options']
                     })
                 for image in item['images']:
                     raw_data['media_gallery_entries'].append({
                         "id": image['id'],
-                        "position": image['position'],
                         "file": image['src'],
-                        "created_at": image['created_at'],
-                        "updated_at": image['updated_at'],
+                        "label": image['name'],
                     })
                 data_convert.append(raw_data)
             return data_convert
         processed_data = response.json()
         res = preprocess_input(processed_data)
-        yield from extract_jsonpath(self.records_jsonpath, input={
-            "products": res
-        })
+        yield from extract_jsonpath(self.records_jsonpath, input={"products": res})
 
 
 class CategoriesStream(WooCommerceStream):
     """Define custom stream."""
     name = "categories"
     path = "products/categories"
+    records_jsonpath = "$.categories[*]"
     primary_keys = ["id"]
     replication_key = None
 
     schema = th.PropertiesList(
         th.Property("id", th.IntegerType),
         th.Property("name", th.StringType),
-        th.Property("slug", th.StringType),
-        th.Property("parent", th.IntegerType),
-        th.Property("description", th.StringType),
-        th.Property("display", th.StringType),
-        th.Property("image", th.ObjectType(
-            th.Property("id", th.IntegerType),
-            th.Property("date_created", th.DateTimeType),
-            th.Property("date_created_gmt", th.DateTimeType),
-            th.Property("date_modified", th.DateTimeType),
-            th.Property("date_modified_gmt", th.DateTimeType),
-            th.Property("src", th.StringType),
-            th.Property("name", th.StringType),
-            th.Property("alt", th.StringType)
-        )),
-        th.Property("menu_order", th.IntegerType),
-        th.Property("count", th.IntegerType),
-        th.Property("_links", th.ObjectType(
-            th.Property("self", th.ArrayType(th.ObjectType(
-                th.Property("href", th.StringType),
-            ))),
-            th.Property("collection", th.ArrayType(th.ObjectType(
-                th.Property("href", th.StringType),
-            ))),
-        ))
+        th.Property("source", th.StringType),
     ).to_dict()
+
+    def parse_response(self, response: requests.Response) -> Iterable[dict]:
+        def preprocess_input(data):
+            data_convert = []
+            for item in data:
+                raw_data = {
+                    "id": item['id'],
+                    "name": item['name'],
+                    "source": "woocommerce",
+                }
+                data_convert.append(raw_data)
+            return data_convert
+        processed_data = response.json()
+        res = preprocess_input(processed_data)
+        yield from extract_jsonpath(self.records_jsonpath, input={"categories": res})
 
 
 class ProductsAttributeStream(WooCommerceStream):
     """Define custom stream."""
     name = "products_attribute"
     path = "products/attributes"
+    records_jsonpath = "$.products_attributes[*]"
     primary_keys = ["id"]
     replication_key = None
 
     schema = th.PropertiesList(
         th.Property("id", th.IntegerType),
-        th.Property("name", th.StringType),
-        th.Property("slug", th.StringType),
-        th.Property("type", th.StringType),
-        th.Property("order_by", th.StringType),
-        th.Property("has_archives", th.BooleanType),
-        th.Property("_links", th.ObjectType(
-            th.Property("self", th.ArrayType(th.ObjectType(
-                th.Property("href", th.StringType),
-            ))),
-            th.Property("collection", th.ArrayType(th.ObjectType(
-                th.Property("href", th.StringType),
-            ))),
-        ))
+        th.Property("attribute_code", th.StringType),
+        th.Property("default_frontend_label", th.StringType),
+        th.Property("default_value", th.StringType),
+        th.Property("source", th.StringType),
     ).to_dict()
+
+    def parse_response(self, response: requests.Response) -> Iterable[dict]:
+        def preprocess_input(data):
+            data_convert = []
+            for item in data:
+                raw_data = {
+                    "id": item['id'],
+                    "attribute_code": item['slug'],
+                    "default_frontend_label": item['name'],
+                    "default_value": item['name'],
+                    "source": "woocommerce",
+                }
+                data_convert.append(raw_data)
+            return data_convert
+        processed_data = response.json()
+        res = preprocess_input(processed_data)
+        yield from extract_jsonpath(self.records_jsonpath, input={"products_attributes": res})
